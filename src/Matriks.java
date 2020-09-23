d82c462549c8f2938046fd2be704ba2e97e15d1b
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2020-09-23 14:59:46 +0800

    Initialize ADT Matriks & Main Program

---
+++ src/Matriks.java
@@ -0,0 +1,58 @@
+package src;
+
+import java.util.*;
+
+public class Matriks {
+    /* Definisi ADT Matriks */
+    /* Attribute */
+    int IdxBrsMin = 0;
+    int IdxBrsMax = 99;
+    int IdxKolMin = 0;
+    int IdxKolMax = 99;
+    int IdxUndef = -999;
+
+    int NBrsEff;
+    int NKolEff;
+
+    float[][] M = new float[IdxBrsMax][IdxKolMax];
+
+    /* Method */
+    /* --- KONSTRUKTOR --- */
+    public Matriks() {
+        // Mendefinisikan matriks kosong
+        this.NBrsEff = 0;
+        this.NKolEff = 0;
+
+        for (int i=0; i <= this.IdxBrsMax; i++){
+            for (int j=0; j <= this.IdxKolMax; j++){
+                this.M[i][j] = 0;
+            }
+        }
+    }
+
+    public void BacaMatriks() {
+        // Membaca input matriks
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan jumlah baris: ");
+        this.NBrsEff = scan.nextInt();
+
+        System.out.print("Masukkan jumlah kolom: ");
+        this.NKolEff = scan.nextInt();
+
+        for (int i=0; i <= this.NBrsEff; i++){
+            for (int j=0; j <= this.NKolEff; j++){
+                this.M[i][j] = 0;
+            }
+        }
+    }
+
+    public void TulisMatriks() {
+        // Menuliskan matriks ke layar
+        for (int i=0; i <= this.NBrsEff; i++){
+            for (int j=0; j <= this.NKolEff; j++){
+                System.out.printf("%.4f ", this.M[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.*;
+
+class Program {
+    public static void main(String[] args) {
+        System.out.println("Test123"); 
+        Matriks M = new Matriks();
+
+        M.BacaMatriks();
+        M.TulisMatriks();
+    }
+}
